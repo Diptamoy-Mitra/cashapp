2d029eac24b530ce0d39355ed55cdfe891c5bc36
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-08-05 16:53:54 +0530

    change error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Home from './components/Home';
 import Investing from './components/Investing';
 import Navbar from './components/Navbar';
+import Others from './components/Others';
 
 function App() {
   return (
     <div className="App text-2xl font-bold">
-     {/* <h1>Hello world</h1> */}
-     <Navbar></Navbar>
-     <Home></Home>
-     <Investing></Investing>
-     
+      {/* <h1>Hello world</h1> */}
+      <Navbar></Navbar>
+      <Home></Home>
+      <Others />
+      <Investing />
     </div>
   );
 }
